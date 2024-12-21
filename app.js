00c21f5b053367a082d476ed492b77c7acf18244
asadullah <mdasadullah0448@gmail.com>
2024-12-21 17:23:33 +0530

    this is demo

--- app.js
+++ app.js
@@ -1,3 +1,7 @@
 const myHeading = document.querySelector('h1');
 myHeading.innerText = 'good morning ';
 
+let heading = document.querySelector("h2");
+heading.addEventListener("click", function () {
+    alert("Ouch! Stop poking me!");
+  });
